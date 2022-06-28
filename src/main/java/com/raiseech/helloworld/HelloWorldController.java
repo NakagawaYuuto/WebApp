b2034d5a08ac637299e10e22e0341cc3a987a5c8
yuutonakagawa <github.nakagawa@gmail.com>
2022-06-28 18:41:28 +0900

    間違い

--- src/main/java/com/raiseech/helloworld/HelloWorldController.java
+++ src/main/java/com/raiseech/helloworld/HelloWorldController.java
@@ -4,33 +4,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-@RestController
+@RestController()
 public class HelloWorldController {
 
+
     @GetMapping("/hello")
-    public String hello(@RequestParam("name") String name) {
-        return "HelloWorld";
+    public String hello(@RequestParam(value = "name", defaultValue = "HelloWorld") String name) {
+        return String.format("おはよう御座います %s", name);
     }
 
-    @GetMapping("/japanese")
-    public String greeting() {
-        return "おはようございます";
-    }
 
-    @GetMapping("/britain")
-    public String britain() {
-        return "Cheers";
-    }
+//    @GetMapping
+//    public String greeting() {
+//        return "おはようございます";
+//    }
+//
+//    @GetMapping
+//    public String britain() {
+//        return "Cheers";
+//    }
+//
+//    @GetMapping("/time")
+//    public String time() {
+//        LocalDateTime nowDateTime = LocalDateTime.now();
+//        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
+//        String dateTimeJp = format.format(nowDateTime);
+//        return dateTimeJp;
+//    }
 
-    @GetMapping("/time")
-    public String time() {
-        LocalDateTime nowDateTime = LocalDateTime.now();
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
-        String dateTimeJp = format.format(nowDateTime);
-        return dateTimeJp;
-    }
+//    @GetMapping("/greetings")
+//    public String greetings(@RequestParam(name = "country", defaultValue = "aaaaa") String id) {
+//        return "おはようございます";
+//    }
+
+//
+//    @GetMapping("/hello")
+//    String index(
+//            @RequestParam(name = "name", value = "name", defaultValue = "no name", required = false) String name,
+//            @RequestParam(name = "id") String id) {
+//        return "ID:" + id + "NAME" + name;
+//    }
+
 
 }
